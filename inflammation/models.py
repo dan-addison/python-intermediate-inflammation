dc3520c2b7433782993a353e4a072a04b13a505e
Daniel Addison <Dan.Addison@astrazeneca.com>
2023-05-16 11:55:12 +0100

    Added Pylint library

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 
 Functions:
@@ -29,7 +29,8 @@ def load_csv(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
 
-    :param data: a 2D array of inflammation data. Each row represents a single patient. Columns represent days 0-40.
+    :param data: a 2D array of inflammation data. Each row represents a single patient.
+    Columns represent days 0-40.
     :returns: An array of the mean inflammation measure across all patients per day
     """
     return np.mean(data, axis=0)
@@ -38,7 +39,8 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: a 2D array of inflammation data. Each row represents a single patient. Columns represent days 0-40.
+    :param data: a 2D array of inflammation data. Each row represents a single patient.
+    Columns represent days 0-40.
     :returns: An array of the max inflammation measure across all patients per day
     """
     return np.max(data, axis=0)
@@ -47,8 +49,8 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: a 2D array of inflammation data. Each row represents a single patient. Columns represent days 0-40.
+    :param data: a 2D array of inflammation data. Each row represents a single patient.
+    Columns represent days 0-40.
     :returns: An array of the min inflammation measure across all patients per day
     """
     return np.min(data, axis=0)
-
